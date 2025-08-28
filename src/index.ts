50ca4a58f0c2ab4ff7ec7c2dae4845f520ecfebb
quentinjuarez <quentin.juarez@hotmail.fr>
2025-08-28 14:36:21 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { Plugin } from 'vite';
+import { generateGlobalTypes, GenerateOptions } from './generate-types';
+
+export default function vitePluginGlobalTypes(
+  options: GenerateOptions
+): Plugin {
+  return {
+    name: 'vite-plugin-global-types',
+    apply: 'serve',
+    configureServer(server) {
+      server.watcher.on('change', () => {
+        generateGlobalTypes(options);
+      });
+    },
+    buildStart() {
+      generateGlobalTypes(options);
+    },
+  };
+}
