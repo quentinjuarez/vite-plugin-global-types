217d433f278cb59b042cbac5e84caa3fa5b8f95d
quentinjuarez <quentin.juarez@hotmail.fr>
2025-08-29 14:05:35 +0200

    fix types

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    exclude: [],
+    exclude: ['example', 'node_modules', 'dist'],
+    coverage: {
+      include: ['src'],
+      exclude: ['example', 'node_modules', 'dist'],
+    },
   },
 });
